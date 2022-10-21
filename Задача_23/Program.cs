47a97861a185f9ced3a4b36f672aa35f32b77225
Ekaterina Voitkovich <styardess@mail.ru>
2022-10-21 20:59:16 +0300

    задача 23

--- Задача_23/Program.cs
+++ Задача_23/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая принимает на вход число N 
+// и выдает таблицу кубов чисел от 1 до N.
+
+void PrintCoob(int n)
+
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine(i * i * i);
+    }
+
+}
+
+
+PrintCoob(3);
