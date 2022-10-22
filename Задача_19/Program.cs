3c22c8cf53df6e6474cdeee7b69161038fda82db
Ekaterina Voitkovich <styardess@mail.ru>
2022-10-22 18:50:23 +0300

    palindrom, krivo & koso

--- Задача_19/Program.cs
+++ Задача_19/Program.cs
@@ -1,2 +1,24 @@
-﻿// 
+﻿// Программа, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+
+
+
+Console.WriteLine("Введите пятизначное число ");
+string num = (Console.ReadLine());
+
+string array = num;
+char[] obrArray = array.ToCharArray();
+
+
+Array.Reverse(obrArray);
+string newArray = new string(obrArray);
+
+if (array == newArray)
+{
+    Console.WriteLine("Это палиндром!");
+}
+if (array != newArray)
+{
+    Console.WriteLine("Это не палиндром!");
+}
 
